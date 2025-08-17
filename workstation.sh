452142e7dac788ca6cc93cccbfeba5ab8d98dce4
Muralisanku <sankumurali4a6@gmail.com>
2025-08-17 15:45:48 +0530

    k8_eksctl

--- workstation.sh
+++ workstation.sh
@@ -68,9 +68,3 @@ curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 
 sudo mv /tmp/eksctl /usr/local/bin
-
-VALIDATE $? "eksctl installation"
-
-sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
-sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
-VALIDATE $? "kubens installation"
