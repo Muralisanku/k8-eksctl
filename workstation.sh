14c3f95431264ec7e59dcdb2ec1f1ee5807bc5d1
Muralisanku <sankumurali4a6@gmail.com>
2025-09-30 21:12:22 +0530

    k8_eksctl

--- workstation.sh
+++ workstation.sh
@@ -73,3 +73,12 @@ VALIDATE $? "eksctl installation"
 sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
 sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 VALIDATE $? "kubens installation"
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+
+chmod 700 get_helm.sh
+
+./get_helm.sh
+
+curl -sS https://webinstall.dev/k9s | bash
+
