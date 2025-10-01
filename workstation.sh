0cfe74ceeae6412c0355e4815163c747449c2e77
Muralisanku <sankumurali4a6@gmail.com>
2025-10-01 20:51:55 +0530

    k8_eksctl

--- workstation.sh
+++ workstation.sh
@@ -80,5 +80,11 @@ chmod 700 get_helm.sh
 
 ./get_helm.sh
 
+helm repo add aws-ebs-csi-driver https://kubernetes-sigs.github.io/aws-ebs-csi-driver
+
+helm repo update
+
+helm upgrade --install aws-ebs-csi-driver --namespace kube-system aws-ebs-csi-driver/aws-ebs-csi-driver
+
 curl -sS https://webinstall.dev/k9s | bash
 
